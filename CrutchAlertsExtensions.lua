9c62ea61c5e7d42d73d8af27a59b569fe4942631
Kyzderp <amurtiger224@gmail.com>
2026-08-18 00:44:08 -0700

    [0.3.0] Bump version

--- CrutchAlertsExtensions.lua
+++ CrutchAlertsExtensions.lua
@@ -4,7 +4,7 @@
 -----------------------------------------------------------
 CrutchAlertsExtensions = {
     name = "CrutchAlertsExtensions",
-    version = "0.2.2",
+    version = "0.3.0",
 
     CIRCLE = "Circle",
     RECTANGLE = "Rectangle",
